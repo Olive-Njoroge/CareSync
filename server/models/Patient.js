2844ec3ba020d0cb045307645fc51dba5aa98a59
Olive Njoroge <njorogeolive001@gmail.com>
2025-06-23 08:21:07 +0300

    Made some changes

--- server/models/Patient.js
+++ server/models/Patient.js
@@ -1,7 +1,16 @@
 const mongoose = require('mongoose');
 const patientSchema = new mongoose.Schema({
     name: {type: String, required: true},
-    phoneNumber: {type: String, required: true},
+    phoneNumber: {type: String, required: true, unique: true,
+                    validate: {
+                        validator: function (v) {
+                            return /^(\+254|0)?7\d{8}$/.test(v); // Kenyan mobile pattern
+                        },
+                        message: props => `${props.value} is not a valid Kenyan phone number!`
+                    }
+    },
+    nationalId: { type: String, unique: true, sparse: true }, // optional but unique if provided
+
     email: String,
     dateOfBirth: {type: Date, required: true},
     address: String,
@@ -18,5 +27,7 @@ const patientSchema = new mongoose.Schema({
 
 }, {timestamps: true}
 );
+
+
 
 module.exports = mongoose.model("Patient", patientSchema)
